716a0ab60dfa958349d66b86d2fd564497f17d01
kanemamine <64197995+kanemamine@users.noreply.github.com>
2023-09-19 12:46:50 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php/*
+$paragraphe = "<h1>Ceci est un site</h1>";
+echo $paragraphe;
+
+$firstName = "Amine";
+$lastName = "Kanem";
+$age = "26" + 1;
+$cityOfBirth = "Tizi Ouzou";
+$countryOfBirth = "Algeria";
+
+echo "Bonjour je m'appel $firstName $lastName, j'ai $age. <br>";
+echo "Je suis né a $cityOfBirth en $countryOfBirth";
+?>
+<br>
+<br>
+<?php
+$grandParents = [];
+for($i=0; $i<=5; $i++){
+    $grandParents[$i] = $i;
+    echo $grandParents[$i];
+};
+
+?>*/
+
+$tableau = ["sam", "ilyes"];
+$tableau .= "Amine";
+
+foreach ($nam as $tableau){
+    echo "<ul>"
+    echo    "<li>$nom</li>"
+    echo "</ul>"
+}
+?>
